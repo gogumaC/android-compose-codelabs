f3454d18d761faf510d5e9cb5a6058fd36abb96d
gogumaC(mac mini) <kimu2371@naver.com>
2023-06-23 10:19:42 +0900

    feat: Add button, Change val->var

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -1,11 +1,26 @@
 package com.codelabs.state
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier:Modifier= Modifier){
-    val count=0
-    Text(text="You've had $count glasses.")
+    Column(
+        modifier = modifier.padding(16.dp)
+    ){
+        var count=0
+        Text(text="You've had $count glasses.")
+        Button(
+            modifier=Modifier.padding(top=8.dp),
+            onClick = {count++}
+        ){
+            Text("Add one")
+        }
+    }
+
 }

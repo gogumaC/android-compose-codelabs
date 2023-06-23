6d951275f107ab17331b95366998fa1a3780354e
gogumaC(mac mini) <kimu2371@naver.com>
2023-06-23 12:09:23 +0900

    feat: Create TaskList - LazyColum -> not saveable when scrolled

---
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/ui/theme/WellnesTask.kt
@@ -0,0 +1,3 @@
+package com.codelabs.state.ui.theme
+
+data class WellnessTask(val id:Int, val label:String)
--- BasicStateCodelab/app/src/main/java/com/codelabs/state/ui/theme/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/ui/theme/WellnessScreen.kt
@@ -1,5 +1,6 @@
 package com.codelabs.state.ui.theme
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -8,7 +9,11 @@ import com.codelabs.state.WaterCounter
 
 @Composable
 fun WellnessScreen(modifier:Modifier=Modifier){
-    StatefulCounter(modifier)
+    Column(modifier=modifier){
+        StatefulCounter()
+        WellnessTaskList()
+    }
+
 }
 
 @Preview(showBackground = true)

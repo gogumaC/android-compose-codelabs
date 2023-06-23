c115c551d9e45bf98d1f89d0905c1f792e19edb1
gogumaC(mac mini) <kimu2371@naver.com>
2023-06-23 11:43:07 +0900

    feat: State hoisting - Seperate WaterCounter stateful / stateless

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/ui/theme/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/ui/theme/WellnessScreen.kt
@@ -3,11 +3,12 @@ package com.codelabs.state.ui.theme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.codelabs.state.StatefulCounter
 import com.codelabs.state.WaterCounter
 
 @Composable
 fun WellnessScreen(modifier:Modifier=Modifier){
-    WaterCounter()
+    StatefulCounter(modifier)
 }
 
 @Preview(showBackground = true)

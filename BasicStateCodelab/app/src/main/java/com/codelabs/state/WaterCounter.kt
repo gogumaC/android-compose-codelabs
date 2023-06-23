3bc2875aef684d2fa32f19092ff1bf6ed776f597
gogumaC(mac mini) <kimu2371@naver.com>
2023-06-23 10:37:09 +0900

    feat: Save state by remember -> Now Counter Available!
    When component recompose, state always inited.
    so we have to remember the state.

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -6,7 +6,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -15,12 +18,12 @@ fun WaterCounter(modifier:Modifier= Modifier){
     Column(
         modifier = modifier.padding(16.dp)
     ){
-        val count: MutableState<Int> = mutableStateOf(0)
-
+        //val count: MutableState<Int> = remember { mutableStateOf(0) }
+        var count by remember {mutableStateOf(0)}
         Text(text="You've had $count glasses.")
         Button(
             modifier=Modifier.padding(top=8.dp),
-            onClick = {count.value++}
+            onClick = {count++}
         ){
             Text("Add one")
         }

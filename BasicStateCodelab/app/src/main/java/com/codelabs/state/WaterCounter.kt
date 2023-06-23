4c60ab0eb8ac63dbcd642b823c3ef0e73a29e0ad
gogumaC(mac mini) <kimu2371@naver.com>
2023-06-23 11:15:36 +0900

    chore: Initialize WaterCount

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -17,37 +17,14 @@ import com.codelabs.state.ui.theme.WellnessTaskItem
 
 @Composable
 fun WaterCounter(modifier:Modifier= Modifier){
-    Column(
-        modifier = modifier.padding(16.dp)
-    ){
-        //val count: MutableState<Int> = remember { mutableStateOf(0) }
-        var count by remember {mutableStateOf(0)}
-        if(count>0) {
-            var showTask by remember{ mutableStateOf(true) }
-            if(showTask){
-                WellnessTaskItem(
-                    taskName = "Have you taken your 15 minute walk today?",
-                    onClose = { showTask=false})
-            }
-            Text(text="You've had $count glasses.")
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by remember { mutableStateOf(0) }
+        if (count > 0) {
+            Text("You've had $count glasses.")
         }
-
-        Row(Modifier.padding(top=8.dp)){
-            Button(
-                modifier=Modifier.padding(top=8.dp),
-                onClick = {count++},
-                enabled = count<10
-            ){
-                Text("Add one")
-            }
-            Button(
-                modifier=Modifier.padding(top=8.dp),
-                onClick = {count=0},
-            ){
-                Text("Clear water count")
-            }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
-
     }
 
 }

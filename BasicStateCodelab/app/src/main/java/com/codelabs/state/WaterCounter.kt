b9e3718eb5501f5c9980559bee7b25c68556042a
gogumaC(mac mini) <kimu2371@naver.com>
2023-06-23 11:20:34 +0900

    feat: Save state in bundle level by rememberSaveable
    remember - save state in recomposition level
    rememberSaveable - save state in bundle level

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -18,7 +19,7 @@ import com.codelabs.state.ui.theme.WellnessTaskItem
 @Composable
 fun WaterCounter(modifier:Modifier= Modifier){
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
             Text("You've had $count glasses.")
         }
